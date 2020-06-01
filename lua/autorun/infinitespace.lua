08ccd7e737d693bc8ba16e0be06328d21369cb9d
NEON725 <NEON725@hotmail.com>
2020-05-31 23:56:31 -0500

    Added missing convar.

--- lua/autorun/infinitespace.lua
+++ lua/autorun/infinitespace.lua
@@ -2,6 +2,7 @@ local DEFAULT_FLAGS=((not client) and FCVAR_ARCHIVE or 0)+FCVAR_REPLICATED+FCVAR
 
 CreateConVar("infinitespace_oxygen_use",5,DEFAULT_FLAGS,"Oxygen breathed per second.",0)
 CreateConVar("infinitespace_percent_livable_oxygen",20,DEFAULT_FLAGS,"Minimum percent oxygen for breathable atmosphere.",0,100)
+CreateConVar("infinitespace_percent_livable_min_pressure",80,DEFAULT_FLAGS,"Minimum percent breathable air pressure.",0,100)
 CreateConVar("infinitespace_suffocation_damage",5,DEFAULT_FLAGS,"Damage per second from suffocation.",0)
 CreateConVar("infinitespace_max_suit_resources",1000,DEFAULT_FLAGS,"Maximum of each essential resource that can be stored in the suit.",0)
 
