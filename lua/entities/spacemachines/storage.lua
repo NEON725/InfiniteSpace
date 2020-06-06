eedd5f1cafaa26df5feda42685231739526c7a14
NEON725 <NEON725@hotmail.com>
2020-06-05 20:48:35 -0500

    Added many new storage models.

--- lua/entities/spacemachines/storage.lua
+++ lua/entities/spacemachines/storage.lua
@@ -4,13 +4,29 @@ local Models=
 {
 	Liquid=
 	{
-		"models/props_borealis/bluebarrel001.mdl",
+		"models/props_farm/oilcan01.mdl",
+		"models/props_farm/oilcan01b.mdl",
+		"models/props_hydro/water_barrel.mdl",
+		"models/props_hydro/water_barrel_cluster.mdl",
+		"models/props_hydro/water_barrel_cluster2.mdl",
+		"models/props_hydro/water_barrel_cluster3.mdl",
+		"models/props_hydro/water_barrel_large.mdl",
 		"models/props_c17/oildrum001.mdl",
 		"models/props_junk/metalgascan.mdl",
+		"models/props_badlands/barrel01.mdl",
+		"models/props_badlands/barrel02.mdl",
+		"models/props_badlands/barrel03.mdl",
+		"models/props_badlands/barrel_flatbed01.mdl",
 		"models/props_wasteland/laundry_washer001a.mdl",
 		"models/props_mining/watertower.mdl",
 		"models/props_wasteland/horizontalcoolingtank04.mdl",
 		"models/props_silhouettes/pipeline_sec1gate.mdl",
+		"models/props_hydro/water_machinery4.mdl",
+		"models/props_hydro/water_machinery2.mdl",
+		"models/props_2fort/tank001.mdl",
+		"models/props_2fort/tank002.mdl",
+		"models/props_trainyard/metal_watertower001.mdl",
+		"models/props_trainyard/metal_watertower002.mdl",
 		"models/props_interiors/BathTub01a.mdl"
 	},
 	Gas=
